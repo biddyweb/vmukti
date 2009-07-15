3a9df91620ec61535f298c4aad6f8c34b10d5513
hardiksa <hardik@vmukti.com>
2009-07-15 19:26:19 +0530

    Header message has been changed, or added

--- VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
+++ VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
@@ -1,4 +1,4 @@
-/* VMukti 1.0 -- An Open Source Unified Communications Engine
+/* VMukti 2.0 -- An Open Source Video Communications Suite
 *
 * Copyright (C) 2008 - 2009, VMukti Solutions Pvt. Ltd.
 *
@@ -12,7 +12,7 @@
 
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
+* the Free Software Foundation, either version 2 of the License, or
 * (at your option) any later version.
 
 * This program is distributed in the hope that it will be useful,
@@ -23,6 +23,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>
 
+*/
 */
 using System;
 using User.Common;

49f791368679b4fef0b9e14e6ce3efe721879d8c
unknown <Administrator@.(none)>
2009-10-02 10:10:19 +0530

    header has been changed. and unnecessary code has been removed.

--- VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
+++ VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
@@ -1,4 +1,4 @@
-/* VMukti 2.0 -- An Open Source Video Communications Suite
+/* VMukti 2.0 -- An Open Source Unified Communications Engine
 *
 * Copyright (C) 2008 - 2009, VMukti Solutions Pvt. Ltd.
 *
@@ -7,20 +7,12 @@
 * See http://www.vmukti.com for more information about
 * the VMukti project. Please do not directly contact
 * any of the maintainers of this project for assistance;
-<<<<<<< HEAD:VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
-* the project provides a web site, forums and mailing lists      
-=======
 * the project provides a web site, forums and mailing lists
->>>>>>> b74131bacb80d82c79711cf70fe93af3fb611b40:VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
 * for your use.
 
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
-<<<<<<< HEAD:VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
 * the Free Software Foundation, either version 3 of the License, or
-=======
-* the Free Software Foundation, either version 2 of the License, or
->>>>>>> b74131bacb80d82c79711cf70fe93af3fb611b40:VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
 * (at your option) any later version.
 
 * This program is distributed in the hope that it will be useful,
@@ -30,12 +22,7 @@
 
 * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>
-<<<<<<< HEAD:VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
- 
-=======
 
-*/
->>>>>>> b74131bacb80d82c79711cf70fe93af3fb611b40:VMuktiModules/Call Center/UserInfo/User.Business/ClsRole.cs
 */
 using System;
 using User.Common;
